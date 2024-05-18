87e6f7d0ceabbcda6d62f53c96eb51eca981c920
Nathan Ward <175123+vine77@users.noreply.github.com>
2024-05-17 17:49:56 -0700

    Add puppeteer example

--- app/api/youtube+api.ts
+++ app/api/youtube+api.ts
@@ -1,3 +1,30 @@
-export function GET(request: Request) {
-  return Response.json({ hello: 'world' })
+import puppeteer from 'puppeteer'
+
+export async function GET(request: Request) {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // Navigate the page to a URL
+  await page.goto('https://developer.chrome.com/')
+
+  // Set screen size
+  await page.setViewport({ height: 1024, width: 1080 })
+
+  // Type into search box
+  await page.type('.devsite-search-field', 'automate beyond recorder')
+
+  // Wait and click on first result
+  const searchResultSelector = '.devsite-result-item-link'
+  await page.waitForSelector(searchResultSelector)
+  await page.click(searchResultSelector)
+
+  // Locate the full title with a unique string
+  const textSelector = await page.waitForSelector('text/Customize and automate')
+  const fullTitle = await textSelector?.evaluate((el) => el.textContent)
+
+  // await browser.close()
+  browser.close()
+
+  return Response.json({ title: `The title of this blog post is ${fullTitle}` })
 }
