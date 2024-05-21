7ee68eeabf184ac88ab958149e8bfc086f7afde8
Nathan Ward <175123+vine77@users.noreply.github.com>
2024-05-21 16:56:33 -0700

    Add simple UI for fetching

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,54 @@
-import { Box, Heading, Link, LinkText, Text } from '@gluestack-ui/themed'
-
-import ReactLogo from '../assets/images/react-logo.svg'
+import {
+  Box,
+  Button,
+  Heading,
+  Link,
+  LinkText,
+  Text,
+} from '@gluestack-ui/themed'
+import { Image } from 'expo-image'
+import { useState } from 'react'
 
 export default function Page() {
+  const [youtubeFeed, setYouTubeFeed] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean | undefined>(undefined)
+
+  async function getYouTubeFeed() {
+    setLoading(true)
+    const feed = await fetch('/api/youtube')
+    const { youtubeFeed } = await feed.json()
+    setYouTubeFeed(youtubeFeed)
+    setLoading(false)
+  }
+
   return (
-    <Box alignItems="center" flex={1} justifyContent="center" marginBottom={80}>
-      <Box>
-        <ReactLogo height={200} width={200} />
-      </Box>
-      <Heading marginBottom={20}>
-        <Text>
-          Edit <Text fontFamily="monospace">app/index.tsx</Text> and save to
-          reload.
-        </Text>
-      </Heading>
-      <Box>
-        <Text>
-          <Link href="https://react.dev/" isExternal>
-            <LinkText>React docs</LinkText>
-          </Link>
-        </Text>
-      </Box>
-      <Box>
+    <Box alignItems="center" flex={1} marginTop={36}>
+      <Image
+        source="https://avatars.githubusercontent.com/u/133729730"
+        style={{ height: 115, width: 115 }}
+      />
+      <Heading marginBottom={16}>YouTube feed</Heading>
+      <Button color="white" marginBottom={16} onPress={getYouTubeFeed}>
+        Fetch feed
+      </Button>
+      {loading === undefined ? null : loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        youtubeFeed.map((link) => (
+          <Box key={link}>
+            <Link href={link} isExternal>
+              <LinkText>{link}</LinkText>
+            </Link>
+          </Box>
+        ))
+      )}
+      {/* <Box>
         <Text>
           <Link href="https://reactnative.dev/" isExternal>
             <LinkText>React Native docs</LinkText>
           </Link>
         </Text>
-      </Box>
-      <Box>
-        <Text>
-          <Link href="https://docs.expo.dev/" isExternal>
-            <LinkText>Expo docs</LinkText>
-          </Link>
-        </Text>
-      </Box>
-      <Box>
-        <Text>
-          <Link href="https://docs.expo.dev/router/introduction/" isExternal>
-            <LinkText>Expo Router docs</LinkText>
-          </Link>
-        </Text>
-      </Box>
-      <Box>
-        <Text>
-          <Link href="https://gluestack.io/ui/" isExternal>
-            <LinkText>Gluestack docs</LinkText>
-          </Link>
-        </Text>
-      </Box>
+      </Box> */}
     </Box>
   )
 }
